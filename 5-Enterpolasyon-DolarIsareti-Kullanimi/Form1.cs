2506f9453efdb05d7d8045fd9f3626aa357ff040
Hüseyin DEMİRDÖĞER <huseyindemirdoger1992@hotmail.com>
2021-02-14 07:16:12 +0300

    Add project files.

---
+++ 5-Enterpolasyon-DolarIsareti-Kullanimi/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _5_Enterpolasyon_DolarIsareti_Kullanimi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int s1, s2, toplam;
+            s1 = 4;
+            s2 = 7;
+            toplam = s1 + s2;
+            // Başta kullanılan $ işareti sayesinde skop alanlarınıza {} veri tipi değişkenleri verebilirsiniz.
+            string message = $"{s1} ile {s2} toplamı {toplam} sayısına eşittir.";
+
+        }
+    }
+}
